eed9d84090a14e85f9d3bcc88a8e7eb385d64832
Michael Sandstedt <michael.sandstedt@gmail.com>
2019-10-06 17:51:30 -0500

    example server stubs

---
+++ examples/example-server.c
@@ -0,0 +1,7 @@
+#include "zcoap-server.h"
+
+static const coap_node_t core = { .name = "core", .GET = &dump_coap_tree };
+static const coap_node_t *wellknown_children[] = { &core, NULL };
+static const coap_node_t wellknown = { .name = ".well-known", .children = wellknown_children  };
+static const coap_node_t *root_children[] = { &wellknown, NULL };
+static const coap_node_t root = { .children = root_children };
