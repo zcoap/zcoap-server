1d3495d10d3d9af626d28d5cdd04406a8364043c
Andy Gikling <asgikling@gmail.com>
2019-10-06 17:47:54 -0500

    Added gitignore and a CMake clean script.

---
+++ clean_cmake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -v 
+
+set -e
+
+rm -f -R ./CMakeFiles
+rm -f -R ./Bin/CMakeFiles
+rm -f CMakeCache.txt
+rm -f cmake_install.cmake
+rm -f Makefile
+rm -f FlirFrameBenchmark.cbp
+rm -f CMakeLists.txt.user
